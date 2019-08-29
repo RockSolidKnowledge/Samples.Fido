b0470ac868fe6d343a4cebd1fff316cf69a7bb27
Scott Brady <scott@scottbrady91.com>
2019-08-29 13:34:24 +0100

    Core quickstart: registration

---
+++ Quickstarts/Core/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Rsk.AspNetCore.Fido;
+using Rsk.AspNetCore.Fido.Models;
+
+namespace Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFidoAuthentication fido;
+
+        public HomeController(IFidoAuthentication fido)
+        {
+            this.fido = fido ?? throw new ArgumentNullException(nameof(fido));
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult StartRegistration() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationModel model)
+        {
+            var challenge = await fido.InitiateRegistration(model.UserId, model.DeviceName);
+
+            return View(challenge);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CompleteRegistration(FidoRegistrationResponse registrationResponse)
+        {
+            var result = await fido.CompleteRegistration(registrationResponse);
+
+            if (result.IsError) return BadRequest(result.ErrorDescription);
+            return Ok();
+        }
+    }
+}
---
+++ Quickstarts/Core/Models/RegistrationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models
+{
+    public class RegistrationModel
+    {
+        public string UserId { get; set; }
+        public string DeviceName { get; set; }
+    }
+}
