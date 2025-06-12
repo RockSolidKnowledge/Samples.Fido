8c2ee9dfb4d593953516f5d6d8c578c76fceee16
Mark Pitt <mark.pitt@rocksolidknowledge.com>
2025-06-12 08:29:06 +0100

    Update Fido package and configure SQLite support (#15)

---
+++ Quickstarts/EntityFramework/Migrations/20250321122211_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FidoKeys",
+                columns: table => new
+                {
+                    CredentialId = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true),
+                    UserHandle = table.Column<string>(type: "TEXT", nullable: true),
+                    DisplayFriendlyName = table.Column<string>(type: "TEXT", nullable: true),
+                    AttestationType = table.Column<int>(type: "INTEGER", nullable: false),
+                    AuthenticatorId = table.Column<string>(type: "TEXT", nullable: true),
+                    AuthenticatorIdType = table.Column<int>(type: "INTEGER", nullable: true),
+                    Counter = table.Column<int>(type: "INTEGER", nullable: false),
+                    KeyType = table.Column<string>(type: "TEXT", nullable: true),
+                    Algorithm = table.Column<string>(type: "TEXT", nullable: true),
+                    CredentialAsJson = table.Column<string>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastUsed = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FidoKeys", x => x.CredentialId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FidoKeys_UserId",
+                table: "FidoKeys",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FidoKeys");
+        }
+    }
+}
